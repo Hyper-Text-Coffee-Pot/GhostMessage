410c22e6d547508f3a7a17c151e8870c4237a120
charles_jennings <charlesjennings@zoho.com>
2024-02-14 20:40:15 -0600

    Added api versioning

---
+++ API/GhostMessage.Api/Controllers/v1/BaseApiController.cs
@@ -0,0 +1,11 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GhostMessage.Api.Controllers.v1
+{
+	[ApiVersion(1.0)]
+	[ApiController]
+	public class BaseApiController : ControllerBase
+	{
+	}
+}
--- API/GhostMessage.Api/Program.cs
+++ API/GhostMessage.Api/Program.cs
@@ -1,25 +1,40 @@
-var builder = WebApplication.CreateBuilder(args);
+using Asp.Versioning;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+internal class Program
 {
-	app.UseSwagger();
-	app.UseSwaggerUI();
+	private static void Main(string[] args)
+	{
+		var builder = WebApplication.CreateBuilder(args);
+
+		// Add services to the container.
+		builder.Services.AddControllers();
+
+		// Add API versioning to the container.
+		// https://github.com/dotnet/aspnet-api-versioning/wiki
+		builder.Services.AddApiVersioning(options =>
+		{
+			options.DefaultApiVersion = new ApiVersion(1, 0);
+			options.AssumeDefaultVersionWhenUnspecified = true;
+			options.ReportApiVersions = true;
+		});
+
+		// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+		builder.Services.AddEndpointsApiExplorer();
+		builder.Services.AddSwaggerGen();
+
+		var app = builder.Build();
+
+		// Configure the HTTP request pipeline.
+		if (app.Environment.IsDevelopment())
+		{
+			app.UseSwagger();
+			app.UseSwaggerUI();
+		}
+
+		app.UseAuthorization();
+
+		app.MapControllers();
+
+		app.Run();
+	}
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
